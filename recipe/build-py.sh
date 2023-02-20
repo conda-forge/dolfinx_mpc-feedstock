91f1510fc33254e66e563add7b5639e7abf91ad1
Min RK <benjaminrk@gmail.com>
2023-02-20 10:02:21 +0100

    locate petsc4py/dolfinx wrappers when cross-compiling
    cmake `python -c` discovery doesn't work, so add includes by hand

--- recipe/build-py.sh
+++ recipe/build-py.sh
@@ -3,6 +3,15 @@ set -eux
 # https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
 export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 
+# cross-compile doesn't allow retrieving include-dirs from Python imports
+# get them ourselves:
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
+  # we need:
+  # dolfinx.wrappers.get_include_path()
+  # petsc4py.get_include()
+  export CXXFLAGS="${CXXFLAGS} -I${SP_DIR}/dolfinx/wrappers -I${SP_DIR}/petsc4py/include"
+fi
+
 export CMAKE_ARGS="${CMAKE_ARGS} -DPython3_FIND_STRATEGY=LOCATION"
 # show compilation commands for easier debugging
 export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_VERBOSE_MAKEFILE=ON"
